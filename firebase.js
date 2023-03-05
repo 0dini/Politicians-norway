ce6a8624a024b203465521ea3fd66f66e286c0f0
0dini <118266812+0dini@users.noreply.github.com>
2023-03-05 22:42:27 +0100

    linked too firestore

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@
         import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
         // TODO: Add SDKs for Firebase products that you want to use
         // https://firebase.google.com/docs/web/setup#available-libraries
-    
+        import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js"
         // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
@@ -15,7 +15,9 @@
         appId: "1:213722503582:web:6d6dfb4b400cd2982f9e8f",
         measurementId: "G-R12CV9Y85H"
         };
-    
+
         // Initialize Firebase
         const app = initializeApp(firebaseConfig);
         const analytics = getAnalytics(app);
+
+        const db = getFirestore();
