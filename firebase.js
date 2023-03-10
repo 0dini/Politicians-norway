07c31be4c9ae98ee4eaa494f76b2cddbdd202337
0dini <118266812+0dini@users.noreply.github.com>
2023-03-10 13:26:54 +0100

    linked firebase to webapp

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-        import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js"
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-        apiKey: "AIzaSyDrr5aYOY9l3MjXAP1VC30sp-Fsv3MLDv0",
-        authDomain: "politikere-sandefjord.firebaseapp.com",
-        projectId: "politikere-sandefjord",
-        storageBucket: "politikere-sandefjord.appspot.com",
-        messagingSenderId: "213722503582",
-        appId: "1:213722503582:web:6d6dfb4b400cd2982f9e8f",
-        measurementId: "G-R12CV9Y85H"
-        };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js"
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCRVwTmf5q-TEbW-mhpCa3Fqyiz-TPf0Lw",
+    authDomain: "politikere-norge.firebaseapp.com",
+    projectId: "politikere-norge",
+    storageBucket: "politikere-norge.appspot.com",
+    messagingSenderId: "566036296357",
+    appId: "1:566036296357:web:e2162f80b46ed9bb51134c"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-
-        const db = getFirestore();
+const db = getFirestore();
